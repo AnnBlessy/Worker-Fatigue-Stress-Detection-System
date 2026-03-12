674ec947302ca60ed7b30a8068c640cbad99fb4c
swathi-janaki <swathivj3@gmail.com>
2026-03-12 09:19:52 +0530

    Change API_BASE_URL to use VITE_API_URL
    Updated API_BASE_URL to use environment variable.

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
-// const API_BASE_URL = 'http://localhost:8000/api';
 const API_BASE_URL = import.meta.env.VITE_API_URL;
+console.log('API_BASE_URL (built):', API_BASE_URL);
+
+
+// const API_BASE_URL = 'http://localhost:8000/api';
+
 
 export const api = {
   // Analyze a single frame
